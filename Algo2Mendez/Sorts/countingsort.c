3893fbc7b3f87cc5fd98b6e34bdd6c5d9d9a1431
ifabregat <ifabregat@fi.uba.ar>
2025-01-15 20:51:06 -0300

    Define type byte for unsigned char

--- Algo2Mendez/Sorts/countingsort.c
+++ Algo2Mendez/Sorts/countingsort.c
@@ -2,27 +2,39 @@
 #include <stdlib.h>
 #include <string.h>
 
-void countingSort(int *vector, size_t n)
+typedef unsigned char byte;
+
+void countingSort(void *array, size_t n, size_t size, int (*key)(const void *))
 {
 	if (n == 0)
 		return;
 
-	int max = vector[0];
-	int min = vector[0];
+	byte *vector = array;
+
+	int min = key(vector);
+	int max = key(vector);
+
 	for (size_t i = 1; i < n; i++) {
-		if (vector[i] > max)
-			max = vector[i];
-		if (vector[i] < min)
-			min = vector[i];
+		int val = key(vector + i * size);
+		if (val > max)
+			max = val;
+		if (val < min)
+			min = val;
 	}
 
 	int range = max - min + 1;
 
 	int *count = (int *)calloc(range, sizeof(int));
-	int *output = (int *)malloc(n * sizeof(int));
+	byte *output = malloc(n * size);
+
+	if (!count || !output) {
+		perror("Error al asignar memoria");
+		exit(EXIT_FAILURE);
+	}
 
 	for (size_t i = 0; i < n; i++) {
-		count[vector[i] - min]++;
+		int val = key(vector + i * size);
+		count[val - min]++;
 	}
 
 	for (int i = 1; i < range; i++) {
@@ -30,31 +42,38 @@ void countingSort(int *vector, size_t n)
 	}
 
 	for (size_t i = n; i > 0; i--) {
-		output[count[vector[i - 1] - min] - 1] = vector[i - 1];
-		count[vector[i - 1] - min]--;
+		int val = key(vector + (i - 1) * size);
+		int pos = count[val - min] - 1;
+		memcpy(output + pos * size, vector + (i - 1) * size, size);
+		count[val - min]--;
 	}
 
-	memcpy(vector, output, n * sizeof(int));
+	memcpy(vector, output, n * size);
 
 	free(count);
 	free(output);
 }
 
+int keyInt(const void *element)
+{
+	return *(int *)element;
+}
+
 int main()
 {
 	int vector[] = { 23, 1, 12, 9, 3, 32, 5, 17 };
-	int n = sizeof(vector) / sizeof(vector[0]);
+	size_t n = sizeof(vector) / sizeof(vector[0]);
 
 	printf("Vector original: ");
-	for (int i = 0; i < n; i++) {
+	for (size_t i = 0; i < n; i++) {
 		printf("%d ", vector[i]);
 	}
 	printf("\n");
 
-	countingSort(vector, n);
+	countingSort(vector, n, sizeof(int), keyInt);
 
 	printf("Vector ordenado: ");
-	for (int i = 0; i < n; i++) {
+	for (size_t i = 0; i < n; i++) {
 		printf("%d ", vector[i]);
 	}
 	printf("\n");
