05e80b2b7b9af222d70de345042d42945bf80673
ifabregat <ifabregat@fi.uba.ar>
2025-01-15 20:45:05 -0300

    Define type byte for unsigned char

--- Algo2Mendez/Sorts/quicksort.c
+++ Algo2Mendez/Sorts/quicksort.c
@@ -2,37 +2,41 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef unsigned char byte;
+
 size_t particion(void *vector, size_t izq, size_t der, size_t size,
 		 int (*cmp)(const void *, const void *))
 {
-	char *i = (char *)vector + izq * size;
-	char *j = (char *)vector + der * size;
-	char *pivote = (char *)malloc(size);
-	memcpy(pivote, i, size);
+	byte *v = (byte *)vector;
+	byte *pivote = v + izq * size;
+	size_t i = izq + 1;
+	size_t j = der;
 
-	while (i < j) {
-		while (i < j && cmp(j, pivote) >= 0) {
-			j -= size;
+	while (i <= j) {
+		while (i <= j && cmp(v + i * size, pivote) <= 0) {
+			i++;
+		}
+
+		while (i <= j && cmp(v + j * size, pivote) > 0) {
+			j--;
 		}
 
 		if (i < j) {
-			memcpy(i, j, size);
-			i += size;
-		}
-
-		while (i < j && cmp(i, pivote) <= 0) {
-			i += size;
-		}
-
-		if (i < j) {
-			memcpy(j, i, size);
-			j -= size;
+			for (size_t k = 0; k < size; k++) {
+				byte temp = v[i * size + k];
+				v[i * size + k] = v[j * size + k];
+				v[j * size + k] = temp;
+			}
 		}
 	}
 
-	memcpy(i, pivote, size);
-	free(pivote);
-	return (i - (char *)vector) / size;
+	for (size_t k = 0; k < size; k++) {
+		byte temp = v[izq * size + k];
+		v[izq * size + k] = v[j * size + k];
+		v[j * size + k] = temp;
+	}
+
+	return j;
 }
 
 void quicksort(void *vector, size_t izq, size_t der, size_t size,
@@ -77,6 +81,8 @@ int main()
 	for (int i = 0; i < ml; i++) {
 		printf("%d ", vector[i]);
 	}
+
+	printf("\n");
 
 	return 0;
 }
